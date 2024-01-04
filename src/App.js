c4884120f3669d940a141bd0b9c66402dc07b98b
Richard Lopez <rl3020@columbia.edu>
2024-01-04 00:55:11 -0500

    Complete wordle! This game now uses both keyboard and mouse inputs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import styles from "./App.module.css";
 import GameGrid from "./components/Grid";
 import Keyboard from "./components/Keyboard";
 import fiveLetterWords from "./5-letter-words";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const getRandomWord = () => {
   const randomIndex = Math.floor(Math.random() * fiveLetterWords.length);
@@ -19,6 +19,146 @@ const Navbar = () => {
   );
 };
 
+const getBoxColor = (currentGuess, keyboardUtilities) => {
+  const getCharCount = () => {
+    let charMap = {};
+    for (let i = 0; i < ANSWER.length; i++) {
+      if (ANSWER[i] in charMap) {
+        charMap[ANSWER[i]] += 1;
+      } else {
+        charMap[ANSWER[i]] = 1;
+      }
+    }
+    return charMap;
+  };
+
+  const getGreens = (currentGuess, answerCharCount, colorResult) => {
+    for (let i = 0; i < 5; i++) {
+      const guessChar = currentGuess[i];
+      const answerChar = ANSWER[i];
+      if (guessChar === answerChar) {
+        colorResult[i] = "G";
+        keyColors[guessChar] = "G";
+        answerCharCount[guessChar] -= 1;
+      }
+    }
+  };
+
+  const getYellows = (currentGuess, answerCharCount, colorResult) => {
+    for (let i = 0; i < 5; i++) {
+      const guessChar = currentGuess[i];
+      const answerChar = ANSWER[i];
+      if (
+        guessChar !== answerChar &&
+        ANSWER.includes(guessChar) &&
+        answerCharCount[guessChar] - 1 >= 0
+      ) {
+        colorResult[i] = "Y";
+        keyColors[guessChar] = "Y";
+        answerCharCount[guessChar] -= 1;
+      }
+    }
+  };
+
+  const getGreys = (currentGuess, colorResult) => {
+    for (let i = 0; i < 5; i++) {
+      if (colorResult[i] === "") {
+        colorResult[i] = "R";
+        const guessChar = currentGuess[i];
+        if (!(guessChar in keyColors)) {
+          keyColors[guessChar] = "R";
+        }
+      }
+    }
+  };
+
+  let answerCharCount = getCharCount(ANSWER);
+  let colorResult = ["", "", "", "", ""];
+  let keyColors = keyboardUtilities.keyColors;
+  const setKeyColors = keyboardUtilities.setKeyColors;
+
+  getGreens(currentGuess, answerCharCount, colorResult, keyColors);
+  getYellows(currentGuess, answerCharCount, colorResult, keyColors);
+  getGreys(currentGuess, colorResult, keyColors);
+  setKeyColors({ ...keyColors });
+  return colorResult.join("");
+};
+
+const checkWord = (currentGuess, gameUtilities, keyboardUtilities) => {
+  const gameGrid = gameUtilities.gameGrid;
+  const guessIndex = gameUtilities.guessIndex;
+  const setGameGrid = gameUtilities.setGameGrid;
+  const setGuessIndex = gameUtilities.setGuessIndex;
+  const setIsGameComplete = gameUtilities.setIsGameComplete;
+  console.log("checking: ", currentGuess, "\nActual word: ", ANSWER);
+  let colorResult = getBoxColor(currentGuess, keyboardUtilities);
+
+  gameGrid[guessIndex]["color"] = colorResult;
+  setGameGrid({ ...gameGrid });
+  setGuessIndex(guessIndex + 1);
+  if (colorResult === "GGGGG") {
+    setIsGameComplete(true);
+  }
+};
+
+const handleEnter = (currentGuess, gameUtilities, keyboardUtilities) => {
+  if (currentGuess.length < 5) {
+    alert("Your guess is incomplete :/");
+  } else {
+    checkWord(currentGuess, gameUtilities, keyboardUtilities);
+  }
+};
+
+const handleDelete = (currentGuess) => {
+  if (currentGuess.length < 0) {
+    return currentGuess;
+  }
+  return currentGuess.slice(0, currentGuess.length - 1);
+};
+
+const handleLetter = (currentGuess, inputLetter) => {
+  if (currentGuess.length === 5) {
+    return currentGuess;
+  }
+  return currentGuess + inputLetter;
+};
+
+const updateGuess = (currentGuess, inputLetter, gameUtilities) => {
+  const gameGrid = gameUtilities.gameGrid;
+  const guessIndex = gameUtilities.guessIndex;
+  const setGameGrid = gameUtilities.setGameGrid;
+  let updatedGuess = "";
+
+  if (inputLetter === "BACKSPACE") {
+    updatedGuess = handleDelete(currentGuess);
+  } else if (inputLetter !== "ENTER" && inputLetter !== "BACKSPACE") {
+    updatedGuess = handleLetter(currentGuess, inputLetter);
+  }
+  console.log("Updating guess to: ", updatedGuess);
+  // Update the gameGrid when processes user input.
+  gameGrid[guessIndex]["word"] = updatedGuess;
+  setGameGrid({ ...gameGrid });
+};
+
+const continueGame = (guessIndex, isGameComplete) => {
+  if (guessIndex > 5) {
+    alert("You tried. Good game lol");
+    return false;
+  } else if (isGameComplete) {
+    alert("You already one. Stop trying so hard.");
+    return false;
+  }
+  return true;
+};
+
+const isValidUserInput = (inputLetter) => {
+  function isLetter(char) {
+    return /^[a-zA-Z]$/.test(char) || char === "Backspace" || char === "Enter";
+  }
+  if (isLetter(inputLetter)) return true;
+  else return false;
+};
+
 function App() {
   const [gameGrid, setGameGrid] = useState({
     0: { word: "", color: "BBBBB" },
@@ -30,24 +170,52 @@ function App() {
   });
   const [guessIndex, setGuessIndex] = useState(0);
   const [isGameComplete, setIsGameComplete] = useState(false);
-
+  const [keyColors, setKeyColors] = useState({});
   const gameUtilities = {
     gameGrid: gameGrid,
     setGameGrid: setGameGrid,
     guessIndex: guessIndex,
     setGuessIndex: setGuessIndex,
+    isGameComplete: isGameComplete,
+    setIsGameComplete: setIsGameComplete,
   };
+  const keyboardUtilities = {
+    keyColors: keyColors,
+    setKeyColors: setKeyColors,
+  };
+
+  const handleUserInput = (inputLetter) => {
+    console.log("User inputed :", inputLetter);
+    if (!continueGame(guessIndex, isGameComplete)) return;
+    if (!isValidUserInput(inputLetter)) return;
+    inputLetter = inputLetter.toUpperCase();
+
+    const currentGuess = gameGrid[guessIndex]["word"];
+
+    if (inputLetter === "ENTER") {
+      handleEnter(currentGuess, gameUtilities, keyboardUtilities);
+    } else {
+      updateGuess(currentGuess, inputLetter, gameUtilities);
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      handleUserInput(event.key);
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  });
 
   return (
     <>
       <Navbar />
       <GameGrid gameGrid={gameGrid} />
-      <Keyboard
-        gameUtilities={gameUtilities}
-        answer={ANSWER}
-        isGameComplete={isGameComplete}
-        setIsGameComplete={setIsGameComplete}
-      />
+      <Keyboard keyColors={keyColors} handleUserInput={handleUserInput} />
     </>
   );
 }
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,141 +1,6 @@
 import styles from "./Keyboard.module.css";
-import { useState } from "react";
 
-const Key = ({
-  letter,
-  gameUtilities,
-  answer,
-  isGameComplete,
-  setIsGameComplete,
-  keyColors,
-  setKeyColors,
-}) => {
-  const gameGrid = gameUtilities.gameGrid;
-  const setGameGrid = gameUtilities.setGameGrid;
-  const guessIndex = gameUtilities.guessIndex;
-  const setGuessIndex = gameUtilities.setGuessIndex;
-
-  const getBoxColor = (currentGuess) => {
-    const getCharCount = (answer) => {
-      let charMap = {};
-      for (let i = 0; i < answer.length; i++) {
-        if (answer[i] in charMap) {
-          charMap[answer[i]] += 1;
-        } else {
-          charMap[answer[i]] = 1;
-        }
-      }
-      return charMap;
-    };
-
-    const getGreens = (answerCharCount, colorResult) => {
-      for (let i = 0; i < 5; i++) {
-        const guessChar = currentGuess[i];
-        const answerChar = answer[i];
-        if (guessChar === answerChar) {
-          colorResult[i] = "G";
-          keyColors[guessChar] = "G";
-          answerCharCount[guessChar] -= 1;
-        }
-      }
-    };
-
-    const getYellows = (answerCharCount, colorResult) => {
-      for (let i = 0; i < 5; i++) {
-        const guessChar = currentGuess[i];
-        const answerChar = answer[i];
-        if (
-          guessChar !== answerChar &&
-          answer.includes(guessChar) &&
-          answerCharCount[guessChar] - 1 >= 0
-        ) {
-          colorResult[i] = "Y";
-          keyColors[guessChar] = "Y";
-          answerCharCount[guessChar] -= 1;
-        }
-      }
-    };
-
-    const getGreys = (colorResult) => {
-      for (let i = 0; i < 5; i++) {
-        if (colorResult[i] === "") {
-          colorResult[i] = "R";
-          const guessChar = currentGuess[i];
-          if (!(guessChar in keyColors)) {
-            keyColors[guessChar] = "R";
-          }
-        }
-      }
-    };
-
-    let answerCharCount = getCharCount(answer);
-    let colorResult = ["", "", "", "", ""];
-    getGreens(answerCharCount, colorResult);
-    getYellows(answerCharCount, colorResult);
-    getGreys(colorResult);
-    setKeyColors({ ...keyColors });
-    return colorResult.join("");
-  };
-
-  const checkWord = (currentGuess) => {
-    console.log("checking: ", currentGuess, "\nActual word: ", answer);
-    let colorResult = getBoxColor(currentGuess);
-
-    gameGrid[guessIndex]["color"] = colorResult;
-    setGameGrid({ ...gameGrid });
-    setGuessIndex(guessIndex + 1);
-    if (colorResult === "GGGGG") {
-      setIsGameComplete(true);
-    }
-  };
-
-  const handleEnter = (currentGuess) => {
-    if (currentGuess.length < 5) {
-      alert("Your guess is incomplete :/");
-    } else {
-      checkWord(currentGuess);
-    }
-  };
-
-  const handleDelete = (currentGuess) => {
-    if (currentGuess.length < 0) {
-      return;
-    }
-
-    let updatedGuess = currentGuess.slice(0, currentGuess.length - 1);
-    gameGrid[guessIndex]["word"] = updatedGuess;
-    setGameGrid({ ...gameGrid });
-  };
-
-  const handleLetter = (currentGuess) => {
-    if (currentGuess.length === 5) {
-      return;
-    }
-
-    let updatedGuess = currentGuess + letter;
-    gameGrid[guessIndex]["word"] = updatedGuess;
-    setGameGrid({ ...gameGrid });
-  };
-
-  const handleClick = () => {
-    if (guessIndex > 5) {
-      alert("You tried. Good game lol");
-      return;
-    } else if (isGameComplete) {
-      alert("You already one. Stop trying so hard.");
-      return;
-    }
-
-    const currentGuess = gameGrid[guessIndex]["word"];
-    if (letter === "ENTER") {
-      handleEnter(currentGuess);
-    } else if (letter === "DELETE") {
-      handleDelete(currentGuess);
-    } else if (letter !== "ENTER" && letter !== "DELETE") {
-      handleLetter(currentGuess);
-    }
-  };
-
+const Key = ({ letter, keyColors, handleUserInput }) => {
   const getKeyColor = () => {
     if (letter in keyColors) {
       switch (keyColors[letter]) {
@@ -152,6 +17,10 @@ const Key = ({
     return "";
   };
 
+  const handleClick = () => {
+    handleUserInput(letter);
+  };
+
   return (
     <button
       className={styles["keyboard-key"]}
@@ -163,28 +32,16 @@ const Key = ({
   );
 };
 
-const KeyboardRow = ({
-  keyRow,
-  gameUtilities,
-  answer,
-  isGameComplete,
-  setIsGameComplete,
-  keyColors,
-  setKeyColors,
-}) => {
+const KeyboardRow = ({ keysInRow, keyColors, handleUserInput }) => {
   return (
     <div className={styles["keyboard-row"]}>
-      {keyRow.map((letter, idx) => {
+      {keysInRow.map((letter, idx) => {
         return (
           <Key
-            key={`key-${idx}`}
+            key={`${letter}-${idx}`}
             letter={letter}
-            gameUtilities={gameUtilities}
-            answer={answer}
-            isGameComplete={isGameComplete}
-            setIsGameComplete={setIsGameComplete}
             keyColors={keyColors}
-            setKeyColors={setKeyColors}
+            handleUserInput={handleUserInput}
           />
         );
       })}
@@ -192,33 +49,22 @@ const KeyboardRow = ({
   );
 };
 
-const Keyboard = ({
-  gameUtilities,
-  answer,
-  isGameComplete,
-  setIsGameComplete,
-}) => {
+const Keyboard = ({ keyColors, handleUserInput }) => {
   const keyboardKeys = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
     ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "DELETE"],
   ];
 
-  const [keyColors, setKeyColors] = useState({});
-
   return (
     <div className={styles["keyboard"]}>
-      {keyboardKeys.map((row, idx) => {
+      {keyboardKeys.map((keysInRow, idx) => {
         return (
           <KeyboardRow
             key={`row-${idx}`}
-            keyRow={row}
-            gameUtilities={gameUtilities}
-            answer={answer}
-            isGameComplete={isGameComplete}
-            setIsGameComplete={setIsGameComplete}
+            keysInRow={keysInRow}
             keyColors={keyColors}
-            setKeyColors={setKeyColors}
+            handleUserInput={handleUserInput}
           />
         );
       })}
